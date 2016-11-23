4dbfe8bbbf5486fbf2c784de53cb795beb0d08bd
HarryPaek <HarryPaek@HarryNote>
2016-11-23 15:39:17 +0900

    Jdbc example code added
    a.Jdbc & Get, Post Method with Servlets
    b.Some pom.xml files were updated

---
+++ WebLessons/src/spms/servlets/MemberAddServlet.java
@@ -0,0 +1,78 @@
+package spms.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MemberAddServlet
+ */
+@WebServlet("/member/add")
+public class MemberAddServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head><title>회원 등록</title></head>");
+		out.println("<body>");
+		out.println("<h1>회원 등록</h1>");
+		out.println("<form action='add' method='post'>");
+		out.println("이름: <input type='text' name='name'><br>");
+		out.println("이메일: <input type='text' name='email'><br>");
+		out.println("암호: <input type='password' name='password'><br>");
+		out.println("<input type='submit' value='추가'>");
+		out.println("<input type='reset' value='취소'>");
+		out.println("</form>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		
+		request.setCharacterEncoding("UTF-8");
+
+	    try {
+			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/studydb", "study", "study");
+			stmt = conn.prepareStatement("INSERT INTO MEMBERS(MNAME, EMAIL, PWD, CRE_DATE, MOD_DATE) VALUES (?, ?, ?, NOW(), NOW());");
+			stmt.setString(1, request.getParameter("name"));
+			stmt.setString(2, request.getParameter("email"));
+			stmt.setString(3, request.getParameter("password"));
+			stmt.executeUpdate();
+			
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("<html>");
+			out.println("<head><title>회원 등록 결과</title></head>");
+			out.println("<body>");
+			out.println("<p>성공적으로 등록되었습니다!</p>");
+			out.println("</body>");
+			out.println("</html>");
+		} catch (Exception e) {
+			throw new ServletException(e);
+		}
+	    finally {
+			try {if(stmt != null) stmt.close();} catch(Exception e) {}
+			try {if(conn != null) conn.close();} catch(Exception e) {}
+		}
+	}
+}
---
+++ WebLessons/src/spms/servlets/MemberListServlet.java
@@ -0,0 +1,60 @@
+package spms.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * Servlet implementation class MemberListServlet
+ */
+@WebServlet("/member/list")
+public class MemberListServlet extends GenericServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see Servlet#service(ServletRequest request, ServletResponse response)
+	 */
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+	    try {
+			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/studydb", "study", "study");
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("select MNO, MNAME, EMAIL, CRE_DATE from MEMBERS order by MNO ASC");
+			
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("<html>");
+			out.println("<head><title>회원목록</title></head>");
+			out.println("<body>");
+			out.println("<h1>회원목록</h1>");
+			out.println("<p><a href='add'>신규 회원</a></p>");
+			while (rs.next()) {
+				out.format("%1$d, %2$s, %3$s, %4$tF %4$tT<br>", rs.getInt("MNO"), rs.getString("MNAME"), rs.getString("EMAIL"), rs.getTimestamp("CRE_DATE"));
+			}
+			out.println("</body>");
+			out.println("</html>");
+		} catch (Exception e) {
+			throw new ServletException(e);
+		}
+	    finally {
+			try {if(rs != null) rs.close();} catch(Exception e) {}
+			try {if(stmt != null) stmt.close();} catch(Exception e) {}
+			try {if(conn != null) conn.close();} catch(Exception e) {}
+		}
+	}
+}
